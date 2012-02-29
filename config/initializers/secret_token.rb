bc13b5c6dbbd24dc01016af4044bb81c96e16739
Ariel Chait <apchait@gmail.com>
2012-02-28 17:31:42 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FormPdf::Application.config.secret_token = 'db710bdd95d8d34cbf7231e26740cf5233bad1d252aa2438931bbbd71629bb02991df56126af4a87750a049616d5f4668dc520fc2c32a2fb7e898216ba0776ef'
