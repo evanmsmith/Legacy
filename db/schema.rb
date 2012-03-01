655d8c7d171b100a030b3d04da32cf2a6cef37fd
Ariel Chait <apchait@gmail.com>
2012-02-29 22:55:34 -0800

    middle of building eolforms with custom labels

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120301053905) do
+ActiveRecord::Schema.define(:version => 20120301064752) do
 
   create_table "eolforms", :force => true do |t|
     t.string   "name"
@@ -65,6 +65,12 @@ ActiveRecord::Schema.define(:version => 20120301053905) do
     t.integer  "user_id"
     t.string   "string"
     t.datetime "date"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "testforms", :force => true do |t|
+    t.binary   "choice"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
